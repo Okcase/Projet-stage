6a49359ffc41fbff0bf5e02d480d0319737f1908
William Brebion <william.brebion@live.fr>
2021-06-14 15:29:10 +0200

    Création du fichier python

---
+++ projet.py
@@ -0,0 +1,10 @@
+"""
+Sujet:
+Développer une petite librairie python permettant de transformer/adapter les 
+différentes données open-data de type LPIS (limite parcelle agricole de la PAC)
+dans un format commun. Avec une nomenclature de culture commune (terme anglais 
+ou scientifique/latin) et pouvant être packagé selon le même dallage et 
+projection que les données Sentinel-2.
+"""
+
+
